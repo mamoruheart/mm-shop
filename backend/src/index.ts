c1f6325fd0d1addc83489172c18111777d96a31c
codegur827 <denn522.guru@gmail.com>
2024-09-09 02:53:16 -0500

    remove unused module in backend - body-parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
-// import bodyParser from "body-parser";
 import session from "express-session";
 import passport from "passport";
 
