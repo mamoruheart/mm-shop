9c5dace50baf147aa43bb97e3ddc067327aed271
codegur827 <denn522.guru@gmail.com>
2024-09-12 05:04:01 -0500

    add client codes, confirmed workiing properly

---
+++ client/app/containers/Dashboard/actions.js
@@ -0,0 +1,7 @@
+import { TOGGLE_DASHBOARD_MENU } from "./constants";
+
+export const toggleDashboardMenu = () => {
+  return {
+    type: TOGGLE_DASHBOARD_MENU
+  };
+};
---
+++ client/app/containers/Homepage/reducer.js
@@ -0,0 +1,15 @@
+import { DEFAULT_ACTION } from "./constants";
+
+const initialState = {};
+
+const homepageReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case DEFAULT_ACTION:
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default homepageReducer;
